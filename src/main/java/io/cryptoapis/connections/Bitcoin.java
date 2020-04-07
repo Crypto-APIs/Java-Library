546d8e017547f5694ba2b3a653139ffbbf94d731
stanimir.barakov <stanimir.barakov@menasoftware.com>
2020-04-07 10:02:21 +0300

    updated version

--- src/main/java/io/cryptoapis/connections/Bitcoin.java
+++ src/main/java/io/cryptoapis/connections/Bitcoin.java
@@ -1,16 +1,23 @@
 package io.cryptoapis.connections;
 
 import io.cryptoapis.blockchains.bitcoin_based.services.BtcTransactionService;
+import io.cryptoapis.blockchains.bitcoin_based.services.BtcWalletService;
 import io.cryptoapis.utils.config.EndpointConfig;
 
 public class Bitcoin extends Bitcoin_Based {
 
     private BtcTransactionService btcTransactionService;
+    private BtcWalletService btcWalletService;
     private Omni_Layer omniLayer;
 
     @Override
     public BtcTransactionService getTransactionService(){
         return btcTransactionService;
+    }
+
+    @Override
+    public BtcWalletService getWalletService(){
+        return btcWalletService;
     }
 
     public Bitcoin(EndpointConfig endpointConfig) {
@@ -26,6 +33,7 @@ public class Bitcoin extends Bitcoin_Based {
         super.initServices(endpointConfig);
         try {
             this.btcTransactionService = getConstructor(BtcTransactionService.class).newInstance(endpointConfig);
+            this.btcWalletService = getConstructor(BtcWalletService.class).newInstance(endpointConfig);
             this.omniLayer = getConstructor(Omni_Layer.class).newInstance(endpointConfig);
         } catch (Exception e) {
             e.printStackTrace();
